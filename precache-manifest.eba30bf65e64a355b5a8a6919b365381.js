cef1804726c407148ba57b0f9bf8dc63125c9802
spanri <kozlova9v@mail.ru>
2020-11-27 01:07:14 +0300

    Updates

---
+++ precache-manifest.eba30bf65e64a355b5a8a6919b365381.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "230f1b925c8298130caf",
+    "url": "/css/app.0305b40f.css"
+  },
+  {
+    "revision": "2f83397a8fb6b73733edf308b4617f38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "230f1b925c8298130caf",
+    "url": "/js/app.5dc0181e.js"
+  },
+  {
+    "revision": "be88a258f0eb2bec63d4",
+    "url": "/js/chunk-vendors.5c107056.js"
+  },
+  {
+    "revision": "cf774462a4acb1972aeec41a3ccf70b0",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
